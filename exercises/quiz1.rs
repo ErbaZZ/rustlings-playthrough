b7e543a27dea751277f6e0d51add938e1535779b
ErbaZZ <weerawat.paw@gmail.com>
2023-04-16 02:17:13 +0700

    Playthrough

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apple_amt: u32) -> u32 {
+    if apple_amt > 40 {
+        apple_amt
+    } else {
+        apple_amt * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
